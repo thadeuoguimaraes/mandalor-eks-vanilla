22aa0c9ff9e75539fbf9da625e16c3ea89b1e9f8
Thadeu <marcio.guimaraes@maistodos.com.br>
2026-06-22 21:35:08 -0300

    fix: remove aws-ebs-csi-driver from addons.tf

--- addons.tf
+++ addons.tf
@@ -36,16 +36,3 @@ resource "aws_eks_addon" "kubeproxy" {
     aws_eks_access_entry.nodes
   ]
 }
-
-resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "aws-ebs-csi-driver"
-
-  addon_version               = var.addon_ebs_csi_version
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-
-  depends_on = [
-    aws_eks_access_entry.nodes
-  ]
-}
