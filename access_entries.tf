287c2a5c106ee45da1eebcb16f8746b59dcbea04
Thadeu <marcio.guimaraes@maistodos.com.br>
2026-07-27 22:28:03 -0300

    fix: bump mandalor-eks-module to v1.4.1 to fix nodes access entry principal
    Módulo apontava principal_arn errado (cluster role) no access entry
    EC2_LINUX dos nodes; v1.4.1 corrige pra nodes role. Remove o
    access_entries.tf duplicado que a raiz mantinha por cima e ajusta
    depends_on dos addons pra module.eks.

--- access_entries.tf
+++ access_entries.tf
@@ -1,9 +1,3 @@
-resource "aws_eks_access_entry" "nodes" {
-  cluster_name  = module.eks.cluster_name
-  principal_arn = module.eks.node_role_arn
-  type          = "EC2_LINUX"
-}
-
 resource "aws_eks_access_entry" "fargate" {
   cluster_name  = module.eks.cluster_name
   principal_arn = aws_iam_role.fargate.arn
--- addons.tf
+++ addons.tf
@@ -7,7 +7,7 @@ resource "aws_eks_addon" "cni" {
   resolve_conflicts_on_update = "OVERWRITE"
 
   depends_on = [
-    aws_eks_access_entry.nodes
+    module.eks
   ]
 }
 
@@ -20,7 +20,7 @@ resource "aws_eks_addon" "coredns" {
   resolve_conflicts_on_update = "OVERWRITE"
 
   depends_on = [
-    aws_eks_access_entry.nodes
+    module.eks
   ]
 }
 
@@ -33,6 +33,6 @@ resource "aws_eks_addon" "kubeproxy" {
   resolve_conflicts_on_update = "OVERWRITE"
 
   depends_on = [
-    aws_eks_access_entry.nodes
+    module.eks
   ]
 }
